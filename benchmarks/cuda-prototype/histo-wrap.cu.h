694a32893b29c0ebf8fbdecc68a62215157774ff
Cosmin E. Oancea <coancea76@yahoo.com>
2019-07-11 13:58:54 +0200

    minor

--- benchmarks/cuda-prototype/histo-wrap.cu.h
+++ benchmarks/cuda-prototype/histo-wrap.cu.h
@@ -115,8 +115,9 @@ locMemHwdAddCoop(AtomicPrim select, const int N, const int H, const int histos_p
     cudaMemset(d_histos, 0, mem_size_histos);
     cudaMemset(d_histo , 0, mem_size_histo );
 
-    const int num_gpu_runs = (((select==XCHG) || (select==CAS)) && (histos_per_block==1)) ?
-                             max(1, GPU_RUNS/25) : GPU_RUNS;
+    const int num_gpu_runs = GPU_RUNS;
+                             //(((select==XCHG) || (select==CAS)) && (histos_per_block==1)) ?
+                             //max(1, GPU_RUNS/25) : GPU_RUNS;
 
     unsigned long int elapsed;
     struct timeval t_start, t_end, t_diff;
