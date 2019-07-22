002d7d22287923f823b590ffb1cd27752fb47c3b
Cosmin E. Oancea <coancea76@yahoo.com>
2019-07-22 19:15:34 +0200

    minor

--- benchmarks/cuda-prototype/histo-main.cu
+++ benchmarks/cuda-prototype/histo-main.cu
@@ -79,15 +79,16 @@ void autoLocSubHistoDeg(const AtomicPrim prim_kind, const int H, const int N, in
     const int elms_per_block = (N + BLOCK - 1) / BLOCK;
     const int el_size_tot = (prim_kind == XCHG)? 2*sizeof(int) : sizeof(int);
     const int el_size = sizeof(int);
-    const float m = MIN((lmem*1.0 / el_size), (float)elms_per_block) / H;
-    if (m < 1.0) {
-        *M = 1;
-//        *num_chunks = ceil(1.0 / m);
-    } else if (prim_kind == ADD) {
-        *num_chunks = 1;
-        *M = min( (int)floor(m), BLOCK );
+
+    float m = MIN((lmem*1.0 / el_size), (float)elms_per_block) / H;
+
+    if (prim_kind == ADD) {
+        *M = max(1, min( (int)floor(m), BLOCK ) );
+//    } else if (m < 1.0) {
+//        *M = 1;
     } else {
         // Cosmin: test if this works reasonably well!
+        m = max(1.0, m);
         const float c = BLOCK / m;
         float tmp = c*RACE_FACT / (m * H);
         ////tmp = (tmp - floor(tmp)) >= 0.5 ? ceil(tmp) : floor(tmp);
