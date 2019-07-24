1390e9dea387aed24d4ea990fdd7b1cd71a4b2d9
Cosmin E. Oancea <coancea76@yahoo.com>
2019-07-24 17:03:21 +0200

    formatting

--- benchmarks/cuda-prototype/histo-wrap.cu.h
+++ benchmarks/cuda-prototype/histo-wrap.cu.h
@@ -325,7 +325,7 @@ void printLaTex( const unsigned long runtimes[3][num_histos][num_m_degs]
             else                       printf("Ours");
             
             for(int i = 0; i<num_histos; i++) {
-                printf("\t& %f.2", runtimes[k][i][j]/1000.0);
+                printf("\t& %3.2f", runtimes[k][i][j]/1000.0);
             }
             printf("\\\\");
             if(j == (num_m_degs-1)) printf("\\hline");
