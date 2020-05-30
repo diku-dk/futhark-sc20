38dfdf3445dec02cdcc39b1fb7e8505264d344a5
Troels Henriksen <athas@sigkill.dk>
2020-05-30 21:03:17 +0200

    Changes.

--- benchmarks/futhark.mk
+++ benchmarks/futhark.mk
@@ -8,7 +8,8 @@ FUTHARK_BACKEND=cuda
 # may want to comment them out when using 'opencl'.
 FUTHARK_BENCH_OPTIONS=--pass-option=--nvrtc-option=-arch=compute_35 --pass-option=--default-num-groups=272
 
-# The 435.gromacs benchmark overrides this because it is too low there.
+# Some benchmarks override this if they run very quickly and need more
+# iterations for stable results.
 FUTHARK_BENCH_RUNS=100
 
 %.json: %.fut
